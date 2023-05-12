b5f3110c9e3d558b03460fa2fbe61614d521e356
Manwe03 <38886746+Manwe03@users.noreply.github.com>
2023-05-12 19:16:07 +0200

    MegaBombaCode
    onCalendarBotton

--- src/javafxmlapplication/FXMLDocumentController.java
+++ src/javafxmlapplication/FXMLDocumentController.java
@@ -4,26 +4,36 @@
  */
 package javafxmlapplication;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.image.Image;
 import javafx.scene.layout.FlowPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Screen;
+import model.Booking;
 import model.Club;
 import model.ClubDAOException;
+import model.Court;
+import model.Member;
 
 
 /**
@@ -34,6 +44,10 @@ import model.ClubDAOException;
 public class FXMLDocumentController implements Initializable {
 
     double dpi;
+    
+    String login;
+    
+    String password;
     
     @FXML
     private ScrollPane scrollPane;
@@ -56,7 +70,7 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private VBox vBoxPista6;
     @FXML
-    private Button b1_9;
+    private Button b1_09;
     @FXML
     private Button b1_10;
     @FXML
@@ -82,7 +96,7 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Button b1_21;
     @FXML
-    private Button b2_9;
+    private Button b2_09;
     @FXML
     private Button b2_10;
     @FXML
@@ -108,7 +122,7 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Button b2_21;
     @FXML
-    private Button b3_9;
+    private Button b3_09;
     @FXML
     private Button b3_10;
     @FXML
@@ -134,7 +148,7 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Button b3_21;
     @FXML
-    private Button b4_9;
+    private Button b4_09;
     @FXML
     private Button b4_10;
     @FXML
@@ -160,7 +174,7 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Button b4_21;
     @FXML
-    private Button b5_9;
+    private Button b5_09;
     @FXML
     private Button b5_10;
     @FXML
@@ -186,7 +200,7 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Button b5_21;
     @FXML
-    private Button b6_9;
+    private Button b6_09;
     @FXML
     private Button b6_10;
     @FXML
@@ -225,19 +239,19 @@ public class FXMLDocumentController implements Initializable {
         tabPane.getSelectionModel().select(2);//pone la tab pistas como seleccion inicial
         
         updateFlowPane();
-        setvBoxPistaInitialSize();        
-        
+        setvBoxPistaInitialSize();    
+
         scrollPane.widthProperty().addListener((observable,oldVal,newVal)-> {//on withpropertie changed
             updateFlowPane();
         });
         
-        //DEFINICIÓN DEL CLUB GREENBALL
-        
+  
         try {
-            Club greenBall = Club.getInstance();    
+            Club.getInstance().setInitialData(); //Resetea la base de datos al iniciar
+            //Club.getInstance().addSimpleData();
+            Club.getInstance().registerMember("pepe", "pipo", "9999999", "pepe", "pipo", "999", 000, null); //registra un miembro de prueba
+        
             
-            
-            System.out.println(Club.getInstance().getCourts().get(0).getName());
             
         } catch (ClubDAOException ex) {
             Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
@@ -265,7 +279,7 @@ public class FXMLDocumentController implements Initializable {
     private void updateFlowPane(){
         double width = scrollPane.getWidth()-15;
         double pistaWidth = vBoxPista1.getWidth();
-
+  
         int columns = (int) Math.floor(width/pistaWidth);
         if(columns < 6){
             flowPane.setMaxWidth(columns*pistaWidth+1);
@@ -275,9 +289,53 @@ public class FXMLDocumentController implements Initializable {
             flowPane.setMinWidth(6*pistaWidth+1);
         }
     }
+    
+    /**Hace la reserva. Devuelve true si se puede hacer la reserva false si no.*/
+    private boolean safeRegisterBooking(LocalDateTime bookingDate, LocalDate madeForDay, LocalTime fromHour, boolean paid, Court court, Member member){
+        try {
+            List<Booking> reservasDelDia = Club.getInstance().getCourtBookings(court.getName(), madeForDay);
+            Boolean reservaDuplicada = false;
+            for(Booking reserva: reservasDelDia){
+                if(reserva.getFromTime().equals(fromHour)){
+                    reservaDuplicada = true;
+                }
+            }
+            
+            if(!reservaDuplicada){ //si se puede reservar a esa hora es dicir no hay una reserva a la misma hora, hace la reserva
+                Club.getInstance().registerBooking(bookingDate, madeForDay, fromHour, paid, court, member);
+                System.out.println("Reserva Exitosa");
+                return true;//exito
+            }
+            else{   //si no se puede hacer la reserva
+                //FALTA avisar al usuario
+                System.out.println("Reserva Fallida");
+                return false;//fallo
+            }
+        } catch (ClubDAOException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;//fallo
+    }
 
     @FXML
-    private void Pb1_9(ActionEvent event) {
-        event.getSource();
+    private void onCalendarButton(ActionEvent event) throws ClubDAOException, IOException {
+        Object testBoton = event.getSource();
+        if(testBoton instanceof Button){
+            Button boton = (Button) testBoton;
+            Court court = Club.getInstance().getCourt("Pista " + boton.getId().substring(1, 2));//Pista sobre la que se a hecho click
+            //System.out.println(boton.getId().substring(3, 5));
+            int horaSeleccionada = Integer.valueOf(boton.getId().substring(3, 5));
+            Member usuario = Club.getInstance().getMemberByCredentials("pepe", "pipo");
+                   
+            if(Club.getInstance().hasCreditCard(usuario.getNickName())){//si tiene tarjeta de credito
+                safeRegisterBooking(LocalDateTime.now(), LocalDate.of(2023, 1, 1), LocalTime.of(horaSeleccionada, 0), true, court, usuario);
+            }
+            else{//si no tiene 
+                
+            }
+        }
+        
     }
 }
