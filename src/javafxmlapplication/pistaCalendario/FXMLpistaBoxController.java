d52568cf8adaa5f0ff8e90f453259bd244fee7db
Manwe03 <38886746+Manwe03@users.noreply.github.com>
2023-05-31 20:12:10 +0200

    hora

--- src/javafxmlapplication/pistaCalendario/FXMLpistaBoxController.java
+++ src/javafxmlapplication/pistaCalendario/FXMLpistaBoxController.java
@@ -19,8 +19,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafxmlapplication.FXMLDocumentController;
 import javafxmlapplication.UtilData;
 import model.Booking;
